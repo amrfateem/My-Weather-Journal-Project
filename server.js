f19e0f903d41934e8e60afaca4cccdd5731209c6
amrfateem <35631863+amrfateem@users.noreply.github.com>
2021-01-09 19:28:17 +0200

    Finished the project
    Added the required node modules.
    Got the Weather API working right.
    Changed some styling.

--- server.js
+++ server.js
@@ -2,18 +2,43 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
+const bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 7423; //it's a code from a game so that i can remember it better
+const server = app.listen(port, () => console.log(`Server is up on ${port}`))
+
+//Simple GET route with name all to send the collected data
+app.get('/all', function (req, res) {
+    res.send(projectData);
+});
+
+//POST route to append data to to ProjectData object
+app.post('/append', collected);
+
+function collected(req, res) {
+    //get all then distrubite them
+    let data = req.body;
+    projectData['temp'] = data.temp;
+    projectData['content'] = data.content;
+    projectData['date'] = data.date;
+};
--- website/app.js
+++ website/app.js
@@ -1,5 +1,79 @@
-/* Global Variables */
+// Global Variables
+const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const myAPIKey = '&appid=74d326e6b8f3299c58112a3758c76c3f&units=metric'; //Metric C*
+const generate = document.getElementById('generate');
+const desiredZipCode = document.getElementById('zip');
+const feelingbox = document.getElementById('feelings');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+//Generate button
+generate.addEventListener('click', getWeather);
+
+function getWeather(e) {
+    e.preventDefault();
+    const zipCode = desiredZipCode.value;
+    const content = feelingbox.value;
+    // Chain promise
+    getZip(baseURL, zipCode, myAPIKey)
+        .then((userData) => {
+            postData('/append', {
+                temp: userData.main.temp,
+                content,
+                date: newDate
+            })
+        })
+        .then(() => {
+            updateUI()
+        });
+
+    // TO clear input upon submitting
+    removeText();
+}
+
+//fetching data using zip code, default is US (tried using EG data but the site seems to not respond to it)
+const getZip = async (baseURL, zipCode, myAPIKey) => {
+    const res = await fetch('http://' + baseURL + zipCode + myAPIKey);
+    try {
+        const userData = await res.json();
+        return userData;
+    } catch (error) {
+        console.log(`error is: ${error}`)
+    }
+}
+
+//Routing the data from Client-side to Server-side
+
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log(`error is: ${error}`)
+    }
+}
+
+//updating the content
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('content').innerHTML = allData.content;
+        document.getElementById('date').innerHTML = allData.date;
+    } catch (err) {
+        console.log(`error is: ${err}`);
+    }
+}
